3f35ed8cbc99bfe20ef462475d327325227a5641
Willy Phelan <phelanguille@gmail.com>
2025-10-22 11:29:50 -0300

    Agrego proyecto Code::Blocks en Carpeta Willy + .gitignore para bin/obj

---
+++ Carpeta Willy/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Persona.h"
+
+using namespace std;
+
+int main(){
+
+    Persona persona1 ;
+
+    Persona persona2 ;
+
+    persona1.mostrar() ;
+
+    persona2.cargar() ;
+
+    persona2.mostrar() ;
+
+
+    return 0;
+}
