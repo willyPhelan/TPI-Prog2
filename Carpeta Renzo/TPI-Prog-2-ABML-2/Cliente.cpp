8c563ce18f0093ce73b704724eb435a53ad2ac4a
Renzo1845 <renzo.termine1965@gmail.com>
2025-10-29 17:03:09 -0300

    ABML con menu

---
+++ Carpeta Renzo/TPI-Prog-2-ABML-2/Cliente.cpp
@@ -0,0 +1,152 @@
+#include "Cliente.h"
+#include <iostream>
+#include <cstring>
+#include "Utils.h"
+#include "ClienteArchivo.h"
+
+using namespace std ;
+
+// Constructores
+
+Cliente::Cliente() : tipo_Cliente(0) { // constructor por defecto
+
+    // Inicializo el array de caracteres 'mail'
+    // La clase base Persona se inicializa con sus valores por defecto.
+
+    strncpy(this->mail, "", 29) ;
+}
+
+Cliente::Cliente(string mail, int tipo_Cliente){
+
+    setMail(mail) ;
+
+    setTipo_Cliente(tipo_Cliente) ;
+}
+
+// Getters
+
+string Cliente::getMail(){
+
+    return string (mail) ;
+
+}
+
+int Cliente::getTipo_Cliente (){
+
+    return tipo_Cliente ;
+
+}
+
+// Setters
+
+void Cliente::setMail(string mail) {
+
+    strncpy(this->mail, mail.c_str(), 29) ;
+}
+
+void Cliente::setTipo_Cliente (int tipo_Cliente){
+
+    this->tipo_Cliente = tipo_Cliente ;
+}
+
+// Metodos
+
+void Cliente::cargar(){
+
+    string datos ;
+    int tipo_Cliente ;
+
+    ClienteArchivo reg;
+
+    cout << "Ingrese el CUIT del cliente: " << endl ;
+
+    cin >> datos ;
+
+    Cliente::setCuit(datos) ;
+
+    cout << "Ingrese el tipo de cliente: " ;
+
+    cin >> tipo_Cliente ;
+
+    Cliente::setTipo_Cliente(tipo_Cliente) ;
+
+
+
+    cout << "Ingrese el nombre del cliente: " << endl ;
+
+    cin >> datos ;
+
+    Cliente::setNombre(datos) ;
+
+    cout << "Ingrese el apellido del cliente: " << endl ;
+
+    cin >> datos ;
+
+    Cliente::setApellido(datos) ;
+
+    cout << "Ingrese el telefono del cliente: " << endl ;
+
+    cin >> datos ;
+
+    Cliente::setTelefono(datos) ;
+
+    cout << "Ingrese la direccion del cliente: " << endl ;
+
+    datos = cargarCadena() ;
+
+    Cliente::setDireccion(datos) ;
+
+    cout << "Ingrese el mail del cliente: "  ;
+
+    cin >> datos ;
+
+    Cliente::setMail(datos) ;
+
+    Cliente::setEstado(true) ;
+
+    Cliente::setID(reg.getCantidadRegistros()+1);
+
+    cout << endl ;
+
+    cout << "El cliente fue agregado con exito. " << endl << endl ;
+
+    system("pause") ;
+
+    system("cls") ;
+}
+
+void Cliente::mostrar(){
+
+    cout << "Informacion del cliente: " << endl ;
+
+    cout << endl ;
+
+    cout << "ID del cliente: " << Cliente::getID() << endl;
+
+    cout << "Nombre y apellido del cliente: " << Cliente::getNombre() << " " << Cliente::getApellido() << endl ;
+
+    cout << "Telefono del cliente: " << Cliente::getTelefono() << endl ;
+
+    cout << "Direccion del cliente: " << Cliente::getDireccion() << endl ;
+
+    cout << "Cuit del cliente: " << Cliente::getCuit() << endl ;
+
+    cout << "Mail del cliente: " << Cliente::getMail() << endl ;
+
+    cout << "Tipo de cliente: " << Cliente::getTipo_Cliente() << endl ;
+
+    if (Cliente::getEstado())
+    {
+        cout << "Estado: Activo " << endl;
+    }else{
+
+        cout << "Estado: Inactivo " << endl;
+    }
+
+    cout << endl ;
+
+ //   system ("pause") ;
+
+  //  system ("cls") ;
+}
+
---
+++ Carpeta Renzo/TPI-Prog-2-ABML-2/ClienteArchivo.cpp
@@ -0,0 +1,148 @@
+#include "ClienteArchivo.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+/// FUNCIONES ABML
+
+bool ClienteArchivo::guardar(const Cliente &reg)
+{
+    FILE *archivo;
+
+    archivo = fopen (archivo_Cliente, "ab");
+
+    if (archivo == nullptr)
+    {
+        cout << "NO SE PUDO CREAR EL ARCHIVO. " << endl;
+        return false;
+    }
+
+    int escribio = fwrite (&reg, sizeof (Cliente), 1, archivo);
+
+    fclose (archivo);
+
+    return escribio;
+}
+
+bool ClienteArchivo::bajaLogica (int id_persona)
+{
+    int pos = buscarPosicion (id_persona);
+
+    if (pos == -1)
+    {
+        cout << "El ID que ingreso es incorrecto. " << endl;
+        system ("pause");
+        return false;
+    }
+    Cliente reg = leer (pos);
+
+    reg.setEstado (false);
+
+    FILE *archivo;
+
+    archivo = fopen (archivo_Cliente, "rb+");
+
+    if (archivo == nullptr)
+    {
+        return false;
+    }
+
+    fseek (archivo, pos * sizeof (Cliente), SEEK_SET);
+
+    int escrito = fwrite (&reg, sizeof (Cliente), 1, archivo);
+
+    fclose (archivo);
+
+    return escrito;
+
+}
+
+Cliente ClienteArchivo::leer (int pos)
+{
+    Cliente reg;
+
+    FILE *archivo;
+
+    archivo = fopen (archivo_Cliente, "rb");
+
+    if (archivo == nullptr)
+    {
+        return reg;
+    }
+
+    fseek (archivo, pos * sizeof (Cliente), SEEK_SET);
+
+    fread (&reg, sizeof (Cliente), 1, archivo);
+
+    fclose (archivo);
+
+    return reg;
+}
+
+bool ClienteArchivo::modificar (const Cliente &reg)
+{
+    int pos = buscarPosicion (reg.getID());
+
+    if (pos == -1)
+    {
+        cout << "El ID ingresado es incorrecto. " << endl;
+        return false;
+    }
+
+    FILE *archivo;
+
+    archivo = fopen (archivo_Cliente, "rb+");
+
+    if (archivo == nullptr)
+    {
+        return false;
+    }
+
+    fseek (archivo, pos * sizeof (Cliente), SEEK_SET);
+
+    int escribio = fwrite (&reg, sizeof (Cliente), 1, archivo);
+
+    fclose (archivo);
+
+    return escribio;
+}
+/// FUNCIONES AUXILIARES
+
+int ClienteArchivo::buscarPosicion (int id_persona)
+{
+    Cliente reg;
+
+    int cantReg = getCantidadRegistros();
+
+    for (int i = 0; i<cantReg; i++)
+    {
+        reg = leer(i);
+
+        if (reg.getID() == id_persona)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int ClienteArchivo::getCantidadRegistros ()
+{
+    FILE *archivo;
+
+    archivo = fopen (archivo_Cliente, "rb");
+
+    if (archivo == nullptr)
+    {
+        return 0;
+    }
+
+    fseek (archivo, 0, SEEK_END);
+
+    int tamanio = ftell (archivo);
+
+    fclose (archivo);
+
+    return tamanio / sizeof (Cliente);
+}
