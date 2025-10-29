916726be214f047b697e59c679fe071eb90afeb4
RamaAlomo <ramiroalomo3@gmail.com>
2025-10-29 17:40:03 -0300

    DetalleVentaArchivo

--- Carpeta Ramiro/DetalleVentaArchivo.h
+++ Carpeta Ramiro/DetalleVentaArchivo.h
@@ -1,1 +1,24 @@
+#pragma once
+#include "Detalle_Venta.h"
+#include <cstdio>
+
+
+class DetalleVentaArchivo
+{
+    private:
+
+    const char* archivo_DetalleVenta = "DetalleVenta.dat";
+
+    public:
+
+        bool guardar(const Detalle_Venta &reg);
+        Detalle_Venta leer(int pos);
+        bool bajaLogica(int idDetalle);
+        bool darDealta(int idDetalle);
+        bool modificar(const Detalle_Venta &reg);
+        int obtenerID();
+        int getCantidadRegistros();
+        int buscarPosicion(int idDetalle);
+
+};
 
