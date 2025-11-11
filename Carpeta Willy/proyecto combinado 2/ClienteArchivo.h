ccf5ac17612cc7e227ec6446c01ce003aae4ef3c
Willy Phelan <phelanguille@gmail.com>
2025-11-11 18:35:20 -0300

    commit 16

---
+++ Carpeta Willy/proyecto combinado 2/ClienteArchivo.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Cliente.h"
+#include <cstdio>
+
+class ClienteArchivo
+{
+
+private: /// Atributos
+
+    const char *archivo_Cliente = "Clientes.dat" ;
+
+public: /// Metodos
+
+/// FUNCIONES ABML
+
+    bool guardar (const Cliente &reg) ;
+
+    bool bajaLogica (int id_persona) ;
+
+    bool altaLogica(int id_persona) ;
+
+    Cliente leer (int pos) ;
+
+    bool modificar (const Cliente &reg) ;
+
+/// FUNCIONES AUXILIARES
+
+    int getCantidadRegistros () ;
+
+    int buscarPosicion (int id_persona) ;
+
+    bool validarCUIT (std::string cuit) ;
+
+    void modificarCampo ();
+
+} ;
+
+
